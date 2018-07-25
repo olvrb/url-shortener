38fa40e093c1ba796a334292bd59b6042b04084f
Oliver Boudet <oboudet1@me.com>
2018-07-25 16:52:37 +0200

    a SHIT TON of refactoring

--- routes/index.js
+++ routes/index.js
@@ -4,68 +4,54 @@ const fs = require("fs");
 const firebase = require("firebase");
 const jsonConfig = require("../config.json");
 const config = {
-  apiKey: jsonConfig.apiKey,
-  authDomain: jsonConfig.authDomain,
-  databaseURL: jsonConfig.databaseURL,
-  storageBucket: jsonConfig.storageBucket
+    apiKey: jsonConfig.apiKey,
+    authDomain: jsonConfig.authDomain,
+    databaseURL: jsonConfig.databaseURL,
+    storageBucket: jsonConfig.storageBucket
 };
 firebase.initializeApp(config);
 const database = firebase.database();
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index");
+    res.render("index");
 });
-router.post("/generateURL", (req, res, next) => {
-  try {
-    let ID = makeid();
-    database.ref("links/" + ID).set({
-      oldURL: req.body.url,
-      ip: req.headers["x-forwarded-for"] || req.connection.remoteAddress,
-      timestamp: +new Date()
-    });
-    let url = req.protocol + "://" + req.get("host") + "/" + ID;
-    res.render("success", {
-      url: url
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-router.get("/api/v1/shortener", (req, res, next) => {
-  let ID = makeid();
-  database.ref("links/" + ID).set({
-    oldURL: req.query.url,
-    ip: req.headers["x-forwarded-for"] || req.connection.remoteAddress,
-    timestamp: +new Date()
-  });
-  let url = req.protocol + "://" + req.get("host") + "/" + ID;
-  res.json({
-    status: 200,
-    data: {
-      link: url
+router.post("/api/v2/generate", (req, res, next) => {
+    try {
+        console.log((req.body));
+        let ID = makeid();
+        database.ref("links/" + ID).set({
+            oldURL: req.body.url,
+            ip: req.headers["x-forwarded-for"] || req.connection.remoteAddress,
+            timestamp: +new Date()
+        });
+        let url = req.protocol + "://" + req.get("host") + "/" + ID;
+        res.status(200).json({
+            url: url,
+            uniq: ID
+        });
+    } catch (error) {
+        console.log(error);
     }
-  });
 });
 router.get("/:param", (req, res, next) => {
-  if (
-    req.url === "/api/v1/shortener" ||
-    req.url === "generateURL" ||
-    req.url === "/"
-  )
-    return;
-  database
-    .ref("links/")
-    .once("value")
-    .then(e => {
-      res.redirect(e.val()[req.url.replace("/", "")].oldURL);
-    });
+    if (
+        req.url === "/api/v2/shortener" ||
+        req.url === "/"
+    )
+        return;
+    database
+        .ref("links/")
+        .once("value")
+        .then(e => {
+            res.redirect(e.val()[req.url.replace("/", "")].oldURL);
+        });
 });
 router.use(function (err, req, res, next) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("404");
-  }
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("404");
+    }
 });
 module.exports = router;
 
@@ -77,11 +63,11 @@ module.exports = router;
  * @returns {String} random id
  */
 function makeid(length = 5) {
-  let text = "";
-  let possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+    let text = "";
+    let possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
